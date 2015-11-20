c9ea8d7120a8236d8806e84a9fdd30b4ba81c912
Andrii Vasyliev <sol@hiqdev.com>
2015-11-20 13:06:17 +0000

    merged

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,8 +3,5 @@
 /// ensure we get report on all possible php errors
 error_reporting(-1);
 
-$_SERVER['SCRIPT_NAME']     = '/' . __DIR__;
-$_SERVER['SCRIPT_FILENAME'] = __FILE__;
 $loader = require_once(__DIR__ . '/../vendor/autoload.php');
 $loader->add('tests\\', __DIR__ . '/../');
-
