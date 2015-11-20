b81be9dc6d46c3b0bbd6614fb196d2395acc6ba0
Andrii Vasyliev <sol@hiqdev.com>
2015-11-20 13:12:15 +0000

    fixed tests bootstrap

--- tests/_bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/// ensure we get report on all possible php errors
-error_reporting(-1);
-
-$loader = require_once(__DIR__ . '/../vendor/autoload.php');
-$loader->add('tests\\', __DIR__ . '/../');
