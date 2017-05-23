02918c0e7fb97cf69a2126717a7a94e7d05667f1
Ilya Rogov <ilyar.software@gmail.com>
2017-05-23 23:40:15 +0300

    Add test coverage for ArrayHelper

--- tests/unit/ArrayHelperTest.php
+++ tests/unit/ArrayHelperTest.php
@@ -18,44 +18,187 @@ use hiqdev\php\collection\ArrayHelper;
 class ArrayHelperTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var Collection
+     * @test
      */
-    protected $sample;
-
-    protected function setUp()
+    public function merge()
     {
-    }
-
-    protected function tearDown()
-    {
-        $this->sample = null;
-    }
-
-    public function testXML()
-    {
-        $xml = simplexml_load_file(__DIR__ . '/test.xml');
-        $a = ArrayHelper::toArray($xml);
-        $this->assertEquals($a, [
-            '@attributes' => [
-                'backupGlobals' => 'false',
-                'backupStaticAttributes' => 'false',
-                'colors' => 'true',
-                'convertErrorsToExceptions' => 'true',
-                'convertNoticesToExceptions' => 'true',
-                'convertWarningsToExceptions' => 'true',
-                'processIsolation' => 'false',
-                'stopOnFailure' => 'false',
-                'syntaxCheck' => 'false',
-                'bootstrap' => './tests/bootstrap.php',
+        $a = [
+            'foo' => 'bar',
+            'bar' => 'baz',
+            'baz' => [
+                'baz-foo' => false,
+                'baz-bar' => false,
             ],
-            'testsuites' => [
-                'testsuite' => [
-                    '@attributes' => [
-                        'name' => 'PhpCollection Unit Test Suite',
-                    ],
-                    'directory' => './tests/',
-                ],
+            'qux' => [
+                'qux-foo',
             ],
-        ]);
+        ];
+        $b = [
+            'bar' => 'qux',
+            'baz' => [
+                'baz-bar' => true,
+            ],
+            'qux' => [
+                'qux-bar',
+            ],
+        ];
+        $c = [
+            'baz' => [
+                'baz-foo' => true,
+            ],
+            'qux' => [
+                'qux-baz',
+            ],
+            'bar',
+            'baz',
+        ];
+        
+        $actual = ArrayHelper::merge($a, $b, $c);
+        $expected = [
+            'foo' => 'bar',
+            'bar' => 'qux',
+            'baz' => [
+                'baz-foo' => true,
+                'baz-bar' => true,
+            ],
+            'qux' => [
+                'qux-foo',
+                'qux-bar',
+                'qux-baz',
+            ],
+            'bar',
+            'baz',
+        ];
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * @test
+     */
+    public function getItems()
+    {
+        $data = [
+            'foo' => 'bar',
+            'bar' => 'qux',
+        ];
+        
+        $actual = ArrayHelper::getItems($data, 'foo');
+        $expected = [
+            'bar' => 'qux',
+        ];
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * @test
+     */
+    public function insertInside()
+    {
+        $data = [
+            'foo' => 'bar',
+            'bar' => 'qux',
+        ];
+        
+        $items = [
+            'bar' => 'foo',
+            'qux' => 'bar',
+        ];
+        
+        $expected = [
+            'foo' => 'bar',
+            'bar' => 'foo',
+            'qux' => 'bar',
+        ];
+        
+        $actual = ArrayHelper::insertInside($data, $items, []);
+        $this->assertEquals($expected, $actual);
+        
+        $actual = ArrayHelper::insertInside($data, $items, ['before' => 'foo']);
+        $this->assertEquals($expected, $actual);
+        
+        $actual = ArrayHelper::insertInside($data, $items, ['after' => 'foo']);
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * @test
+     */
+    public function unique()
+    {
+        $data = [
+            'foo',
+            'foo',
+            'bar',
+            [
+                'foo' => 'bar',
+                'foo' => 'bar',
+            ],
+        ];
+        
+        $actual = ArrayHelper::unique($data);
+        $expected = [
+            0 => 'foo',
+            2 => 'bar',
+            3 => [
+                'foo' => 'bar',
+            ],
+        ];
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * @test
+     */
+    public function uniqueFlat()
+    {
+        $data = [
+            'foo',
+            'foo',
+            'bar',
+            [
+                'foo',
+                'foo',
+            ],
+        ];
+        
+        $actual = ArrayHelper::uniqueFlat($data);
+        $expected = [
+            0 => 'foo',
+            2 => 'bar',
+            3 => [
+                'foo',
+                'foo',
+            ],
+        ];
+        
+        $this->assertEquals($expected, $actual);
+    }
+    
+    /**
+     * @test
+     */
+    public function toArray()
+    {
+        $data = (object)[
+            'foo' => false,
+            'bar' => (object)[
+                'foo' => 'bar',
+                'bar' => true,
+            ],
+        ];
+        
+        $actual = ArrayHelper::toArray($data);
+        $expected = [
+            'foo' => false,
+            'bar' => [
+                'foo' => 'bar',
+                'bar' => true,
+            ],
+        ];
+        
+        $this->assertEquals($expected, $actual);
     }
 }
