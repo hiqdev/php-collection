2f0deef216055250293a7a7fa78d11221b4ff243
Andrii Vasyliev <sol@hiqdev.com>
2017-05-24 10:55:40 +0200

    csfixed

--- tests/unit/CollectionTest.php
+++ tests/unit/CollectionTest.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Collection library for PHP
+ * Collection library for PHP.
  *
  * @link      https://github.com/hiqdev/php-collection
  * @package   php-collection
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\php\collection\tests\unit;
